989534a7bb9b7372da515449e563b74c5983caf7
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-11-01 00:49:44 +0700

    Simplify login auth

--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -4,6 +4,6 @@ const { logoutController } = require('../controllers/logInOutControllers');
 const router = express.Router();
 
 // Logout user
-router.get('/', logoutController);
+// router.get('/', logoutController);
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,8 @@ router.get('/', (req, res) => {
 const loginRoute = require('./loginRoute');
 router.use('/login', loginRoute);
 
-const logoutRoute = require('./logoutRoute');
-router.use('/logout', logoutRoute);
+// const logoutRoute = require('./logoutRoute');
+// router.use('/logout', logoutRoute);
 
 const usersRoute = require('./usersRoute');
 router.use('/users', auth, usersRoute);
