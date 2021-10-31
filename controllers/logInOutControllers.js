f846069c9894a30b19338aa208b29e630acf91fa
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-11-01 00:46:14 +0700

    Simplify login auth

--- controllers/logInOutControllers.js
+++ controllers/logInOutControllers.js
@@ -1,5 +1,4 @@
 const { authLogin, generateToken } = require('../auth/auth');
-const session = require('express-session');
 const { loginValidation } = require('../auth/validations');
 
 const loginController = (req, res) => {
@@ -8,24 +7,19 @@ const loginController = (req, res) => {
 
     authLogin(req.body.username, req.body.password, (err, user) => {
         if (user) {
-            req.session.regenerate(() => {
-                req.session.user = user;
-                req.session.success = `Authenticated as ${req.body.username}`;
-                const token = generateToken(user._id);
-                res.header('token', token);
-                res.json({ message: `success login as ${req.body.username}` });
-            });
+            const token = generateToken(user._id, user.role);
+            res.header('token', token);
+            res.json({ message: `success login as ${req.body.username}` });
         } else {
-            req.session.error = `authentication failed: ${err}`;
             res.json({ message: `login failed: ${err}` });
         }
     });
 };
 
-const logoutController = (req, res) => {
-    req.session.destroy(() => {
-        res.json({ message: 'logged out' });
-    });
-};
+// const logoutController = (req, res) => {
+//     req.session.destroy(() => {
+//         res.json({ message: 'logged out' });
+//     });
+// };
 
-module.exports = { loginController, logoutController };
+module.exports = { loginController };
