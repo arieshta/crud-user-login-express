dfa9a8b4c102cc9c7139059f41c36b4734bf9aba
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-11-01 17:47:32 +0700

    Clear redundant files

--- routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginController } = require('../controllers/logInOutControllers');
-
-const router = express.Router();
-
-// Login user
-router.post('/', loginController);
-
-module.exports = router;
--- routes/logoutRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { logoutController } = require('../controllers/logInOutControllers');
-
-const router = express.Router();
-
-// Logout user
-// router.get('/', logoutController);
-
-module.exports = router;
